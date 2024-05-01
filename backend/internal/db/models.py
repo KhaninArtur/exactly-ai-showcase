1946899b8272df9412a2db9ab68564d0fcf2fc68
KhaninArtur <haninarthur@gmail.com>
2024-05-01 12:36:56 +0200

    Adjusted database connection config

--- backend/internal/db/models.py
+++ backend/internal/db/models.py
@@ -1,14 +1,25 @@
 from datetime import datetime
 from enum import Enum
 
-from sqlalchemy import create_engine, String, Column, DateTime, Enum as SQLEnum, Index
+from sqlalchemy import (
+    create_engine,
+    String,
+    Column,
+    DateTime,
+    Enum as SQLEnum,
+    Index,
+    NullPool,
+)
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from internal.settings import settings
 
 engine = create_engine(
     settings.database_url,
-    pool_size=10,
+    connect_args={
+        "check_same_thread": False
+    },  # Allow use of the DB with multiple threads
+    poolclass=NullPool,  # Disable pooling
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
